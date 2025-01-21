a830d91d0787cd58fc6d5d80e2dc1c73adc262c9
Lee Gyumin <legm0310@gmail.com>
2025-01-21 23:32:10 +0900

    Refactor: 문자열 상수 분리

--- types/literal.go
+++ types/literal.go
@@ -1,3 +1,9 @@
 package types
 
 const PkNil = "private key is nil"
+
+const (
+	CreateWallet = "CreateWallet"
+	TransferCoin = "TransferCoin"
+	MintCoin     = "MintCoin"
+)
--- types/types.go
+++ types/types.go
@@ -1,12 +1,13 @@
 package types
 
-const (
-	CreateWallet = "CreateWallet"
-	TransferCoin = "TransferCoin"
-	MintCoin     = "MintCoin"
-)
-
 type Wallet struct {
-	PrivateKey string `json:"privateKey"`
-	PublicKey  string `json:"publicKey"`
+	PrivateKey string `json:"privateKey" bson:"privateKey"`
+	PublicKey  string `json:"publicKey" bson:"publicKey"`
+	Time       uint64 `json:"time" bson:"time"`
+}
+
+type Tx struct {
+	Hash      string `json:"hash" bson:"hash"`
+	PrevHash  string `json:"prevHash" bson:"prevHash"`
+	Timestamp uint64 `json:"timestamp" bson:"timestamp"`
 }
