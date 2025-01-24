766d32f7b0d5c9db5065d2631ceb7d61a7b3697e
Lee Gyumin <legm0310@gmail.com>
2025-01-25 02:50:14 +0900

    Refactor: 머클트리 노드 포인터 타입으로 변경, 주석 추가

--- service/tree.go
+++ service/tree.go
@@ -15,6 +15,7 @@ type MerkleNode struct {
 func NewMerkleNode(left, right *MerkleNode, data []byte) *MerkleNode {
 	n := MerkleNode{}
 	var hash [32]byte
+	
 	if left == nil && right == nil {
 		hash = sha256.Sum256(data)
 		n.Data = hash[:]
@@ -31,11 +32,11 @@ func NewMerkleNode(left, right *MerkleNode, data []byte) *MerkleNode {
 }
 
 func NewMerkleTree(data [][]byte) *MerkleTree {
-	var nodes []MerkleNode
+	var nodes []*MerkleNode
 
 	for _, d := range data {
 		node := NewMerkleNode(nil, nil, d)
-		nodes = append(nodes, *node)
+		nodes = append(nodes, node)
 	}
 
 	if len(nodes) == 0 {
@@ -43,19 +44,21 @@ func NewMerkleTree(data [][]byte) *MerkleTree {
 	}
 
 	for len(nodes) > 1 {
-		if len(nodes)%2 == 0 {
+		//노드 홀수개 일 때 완전이진트리 구조를 맞춰줌
+		if len(nodes)%2 != 0 {
 			nodes = append(nodes, nodes[len(nodes)-1])
 		}
 
-		var level []MerkleNode
+		var level []*MerkleNode
 
+		//level - 각 level 마다 이전 좌, 우 노드 해시와 좌우를 합쳐 만든 해시를 넣어서 새 노드를 생성. 이후 level 배열에 추가
 		for i := 0; i < len(nodes); i += 2 {
-			node := NewMerkleNode(&nodes[i], &nodes[i+1], nil)
-			level = append(level, *node)
+			node := NewMerkleNode(nodes[i], nodes[i+1], nil)
+			level = append(level, node)
 		}
 
 		nodes = level
 	}
 
-	return &MerkleTree{&nodes[0]}
+	return &MerkleTree{nodes[0]}
 }
